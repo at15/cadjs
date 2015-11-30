d49b50ba913f2a0c26e68f5a820ba4e475ac8231
at15 <at15@outlook.com>
2015-11-30 14:18:29 +0800

    Angular controller is not working

--- lib/app.ts
+++ lib/app.ts
@@ -1,7 +1,10 @@
 /**
  * Created by gpl on 15/11/30.
  */
-
-angular.module('cad',[]);
+angular.module('cad', []);
+//.controller('DebugCtrl',['scope', ($scope) => new App.Controllers.DebugCtrl($scope)]);
+angular.module('cad')
+    .controller('DebugCtrl', DebugCtrl);
+    //.controller('DebugCtrl', ['scope', App.Controllers.DebugCtrl22]);
 
 // just some comment to test watch
---
+++ lib/ctrl/debug-ctrl.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by gpl on 15/11/30.
+ */
+class DebugCtrl {
+    //scope:any;
+    static $inject = ['$scope'];
+
+    //constructor($scope:ng.IScope) {
+    //    this.scope = $scope;
+    //}
+    constructor() {
+
+    }
+}
