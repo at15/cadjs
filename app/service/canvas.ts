51b794ba8a925856d586ea38c57c781561bbc0ac
at15 <at15@outlook.com>
2015-11-30 17:02:07 +0800

    Comment out raphel use fabric.js instead

--- app/service/canvas.ts
+++ app/service/canvas.ts
@@ -1,13 +1,23 @@
 module App.Services {
     import Logger = App.Services.Logger;
     export class Canvas {
-        private logger:Logger;
-
         static $inject = ['Logger'];
+
+        private logger:Logger;
+        //private paper:RaphaelPaper;
+        //private paperId = "paper";
+        //private paperWidth = 500;
+        //private paperHeight = 500;
 
         constructor(logger:Logger) {
             this.logger = logger;
-            this.logger.log('canvas service init')
+            this.logger.log('canvas service init');
+
+            //// init the real canvas
+            //this.paper = Raphael(document.getElementById(this.paperId),
+            //    this.paperWidth, this.paperHeight);
+            //// TODO: check if the logger truly init
+            //this.logger.log('paper init');
         }
 
     }
