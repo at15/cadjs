3644ebe8afbf3563beba2d608e2906805f830ff5
at15 <at15@outlook.com>
2015-11-30 12:18:37 +0800

    Start switching to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: 'style!css' }
+            {test: /\.css$/, loader: 'style!css'}
         ]
     }
 };
