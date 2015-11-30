a656422edbe7101495728010a73f57b7550582d7
at15 <at15@outlook.com>
2015-11-30 14:27:06 +0800

    Fix controller not defined problem
    was using the old bundle.js ....

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,15 @@
 /**
  * Created by gpl on 15/11/30.
  */
+
 angular.module('cad', []);
 //.controller('DebugCtrl',['scope', ($scope) => new App.Controllers.DebugCtrl($scope)]);
 angular.module('cad')
-    .controller('DebugCtrl', DebugCtrl);
-    //.controller('DebugCtrl', ['scope', App.Controllers.DebugCtrl22]);
+    //.controller('DebugCtrl', DebugCtrl);
+    //.controller('DebugCtrl', TestCtrl);
+    .controller('DebugCtrl', App.Controllers.DebugCtrl);
+//.controller('DebugCtrl', function ($scope) {
+//    $scope.data = {message: 'Hello'};
+//});
 
 // just some comment to test watch
--- lib/ctrl/debug-ctrl.ts
+++ lib/ctrl/debug-ctrl.ts
@@ -1,14 +1,20 @@
 /**
  * Created by gpl on 15/11/30.
  */
-class DebugCtrl {
-    //scope:any;
-    static $inject = ['$scope'];
+module App.Controllers {
+    export class DebugCtrl {
+        //scope:any;
+        public title = 'debug';
+        static $inject = ['$scope'];
 
-    //constructor($scope:ng.IScope) {
-    //    this.scope = $scope;
-    //}
-    constructor() {
+        //constructor($scope:ng.IScope) {
+        //    this.scope = $scope;
+        //}
+        constructor() {
+        }
 
+        public ping() {
+            this.title = 'ping';
+        }
     }
 }
